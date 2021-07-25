a07fe1905a07147b0c5c17000e2b044d4ed9c005
SmartPolarBear <CleverCoolBear@outlook.com>
2021-07-25 16:23:44 +0800

    chore(asan): add configuration for asan

--- plane-ticket/about_dlg.h
+++ plane-ticket/about_dlg.h
@@ -1,6 +1,4 @@
 #pragma once
 #include "framework.h"
 
-
-
 DWORD show_about_dialog(HWND h_parent);
--- plane-ticket/helper.h
+++ plane-ticket/helper.h
@@ -6,4 +6,6 @@
 
 #define ARRAY_SIZE(arr) (sizeof((arr))/sizeof((arr)[0]))
 
+/// @brief compute the hash of a string
+/// @return 64bit hashing of the string
 uint64_t hashing(wchar_t* str);
